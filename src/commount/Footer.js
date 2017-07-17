dc8af0ca0a4a426fb8fc0350a1312cf690d88cd2
unknown <>
2017-07-17 23:14:54 +0800

    night

--- src/commount/Footer.js
+++ src/commount/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component{
             <div style={{padding:"20px",textAlign:"center"}}><p>RSS | 源码地址
             CNode 社区为国内最专业的 Node.js 开源技术社区，致力于 Node.js 的技术研究。</p>
 
-            <p>服务器赞助商为  ucloud ，存储赞助商为  七牛云存储 ，由  alinode 提供应用性能服务。</p>
+            <p>服务器赞助商为  ucloud 存储赞助商为  七牛云存储 ，由  alinode 提供应用性能服务。</p>
            <p> 新手搭建 Node.js 服务器，推荐使用无需备案的 DigitalOcean(https://www.digitalocean.com/)</p></div>
         )
     }
